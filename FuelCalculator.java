e910037d63e09958b4183865143895d471d4e643
kamalov <kamalov@ufanet.ru>
2015-11-12 13:05:15 +0500

    program code kamalov

--- FuelCalculator.java
+++ FuelCalculator.java
@@ -59,4 +59,29 @@ public void actionPerformed(ActionEvent ae) {
             }
         });
         resultLabel = new JLabel("Введите данные для рассчёта");
-           
+            GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.ipadx = 90;
+        constraints.gridy = 0;
+        layout.setConstraints(wayLabel, constraints);
+        constraints.gridy = 1;
+        layout.setConstraints(averageLabel, constraints);
+        constraints.gridy = 2;
+        layout.setConstraints(priceLabel, constraints);
+        
+        constraints.gridx = 1;
+        constraints.anchor = GridBagConstraints.EAST;
+        constraints.ipadx = 50;
+        constraints.gridy = 0;
+        layout.setConstraints(wayField, constraints);
+        constraints.gridy = 1;
+        layout.setConstraints(averageField, constraints);
+        constraints.gridy = 2;
+        layout.setConstraints(priceField, constraints);
+        
